7e4a830dcce57a851e787e1d7a7e4907efc16e9b
mantertius <mpat@ic.ufal.br>
2025-07-25 11:04:55 -0300

    task02 resolution

--- tasks/task-02-salt-and-pepper/task-02-salt-and-pepper.py
+++ tasks/task-02-salt-and-pepper/task-02-salt-and-pepper.py
@@ -2,27 +2,34 @@ import numpy as np
 
 def create_salt_and_pepper_noise(height=100, width=100, salt_prob=0.05, pepper_prob=0.05):
     """
-    Returns an image ∈ [-1, 1] containing salt (I = 1.0) and 
+    Returns an image ∈ [-1, 1] containing salt (I = 1.0) and
     pepper (I = -1.0) noise with respective probability distributions
     equal to salt_prob and pepper_prob. Pixels without noise have values of 0.5.
     """
-    ### START CODE HERE ###
-    # TODO
-    ### END CODE HERE ###
+    img = np.full((height, width), 0.5, dtype=np.float32)
+    random_vals = np.random.rand(height, width)
+
+    salt_mask = random_vals < salt_prob
+    img[salt_mask] = 1.0
+
+    pepper_mask = random_vals >= (1 - pepper_prob)
+    img[pepper_mask] = -1.0
+
     return img
 
+
 def main():
-    
+
     img = create_salt_and_pepper_noise(100, 100, 0.1, 0.1)
-    
+
     salt_count = np.sum(img == 1.0)
     pepper_count = np.sum(img == -1.0)
-    
+
     print(f"Salt pixels: {salt_count}, Pepper pixels: {pepper_count}")
-    
+
     assert 900 <= salt_count <= 1100, "Salt pixel count is outside expected range."
     assert 900 <= pepper_count <= 1100, "Pepper pixel count is outside expected range."
-    
+
     print("Test passed!")
 
 
