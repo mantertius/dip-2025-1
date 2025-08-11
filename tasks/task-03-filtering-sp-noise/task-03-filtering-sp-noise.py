070aefb4fe77d0d398439be21304f716330c34ea
mantertius <mpat@ic.ufal.br>
2025-08-10 23:27:45 -0300

    ajustes

--- tasks/task-03-filtering-sp-noise/task-03-filtering-sp-noise.py
+++ tasks/task-03-filtering-sp-noise/task-03-filtering-sp-noise.py
@@ -15,11 +15,11 @@ def remove_salt_and_pepper_noise(image: np.ndarray) -> np.ndarray:
 
     #as salt and pepper is a noise based on dark and brigth pixels, we can use a median filter.
 
-    image = cv2.medianBlur(image, 3)
+    image = cv2.medianBlur(image, 5)
 
     return image
 
 if __name__ == "__main__":
-    noisy_image = cv2.imread("noisy_image.png", cv2.IMREAD_GRAYSCALE)
+    noisy_image = cv2.imread(r"C:\Users\manoe\OneDrive\Desktop\Programming\dip-2025-1\img\head.png", cv2.IMREAD_GRAYSCALE)
     denoised_image = remove_salt_and_pepper_noise(noisy_image)
     cv2.imwrite("denoised_image.png", denoised_image)
